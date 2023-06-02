a0c06a4a96a76c001fe1acfebbb02ac9b7d3c889
abror <inagamovzaynobiddin@gamil.com>
2023-06-02 18:24:54 +0500

    nimadir

--- handlers/button_handlers.py
+++ handlers/button_handlers.py
@@ -1,10 +1,11 @@
 from aiogram import Router
-from aiogram.filters import Text, Command
+from aiogram.filters import Text, Command, or_f
 from aiogram.fsm.context import FSMContext
 from aiogram.types import FSInputFile, ReplyKeyboardRemove
 from aiogram.types import Message
 
-from keyboards.default import back_to_main, main_menu
+from keyboards.default import back_to_main, main_menu, panel_menu, panel_channel, panel_mandatory, panel_referal, \
+    passenger_menu, driver_menu, driver_accaunt, list_reg, region
 
 buttons_router = Router()
 
@@ -20,7 +21,7 @@ async def start(message: Message):
 
 @buttons_router.message(Text('🔝 Asosiy Menyu'))
 async def start(message: Message):
-    photo = FSInputFile('/home/user/projects/aiogram_projects/taksi_bot/images/taksi.jpeg')
+    photo = FSInputFile('/home/abror/Taxi_Bot_musharraf/images/taksi.jpeg')
     await message.answer_photo(
         caption=f"""😊 Assalomu alaykum {message.from_user.full_name} ,
 ❗️ Taxi botimizga xush kelibsiz, o'zingizga maqul bo'limni tanlang""",
@@ -36,4 +37,128 @@ async def cmd_cancel(message: Message, state: FSMContext):
     await message.answer(
         text="Bekor qilindi!",
         reply_markup=ReplyKeyboardRemove()
+    )
+
+
+'Panel'
+
+
+@buttons_router.message(Text('🔰 Panel'))
+async def start(message: Message):
+    await message.answer(
+        f"😊 Admin panelga xush kelibsiz hurmatli {message.from_user.full_name}",
+        # noqa
+        reply_markup=panel_menu.as_markup(resize_keyboard=True)
+    )
+
+
+@buttons_router.message(Text('📈 Kanal / Guruh'))
+async def start(message: Message):
+    await message.answer(
+        "Kanal va guruhlarni qo'shish orqali siz mablag' yig'ishingiz mumkin",  # noqa
+        reply_markup=panel_channel.as_markup(resize_keyboard=True)
+    )
+
+
+@buttons_router.message(Text("Kanal qo'shish"))
+async def start(message: Message):
+    await message.answer(
+        "O'z kanalingizga ushbu @uzb_taksi_bot botni admin qilib barcha huquqlarni bering va biror xabarni shu yerga (forward shaklda) yuboring yoki kanalingiz ID raqamini yuboring yoki kanalingiz telegram manzilini yuboring.",
+        # noqa
+    )
+
+
+@buttons_router.message(Text("Guruh qo'shish"))
+async def start(message: Message):
+    await message.answer(
+        "Kanalingiz qaysi viloyatlar aro ish faoliyatini olib boradi?"
+        # noqa
+    )
+
+
+@buttons_router.message(Text("♾ Majburiy a'zo bot"))
+async def start(message: Message):
+    await message.answer(
+        "Ushbu bo'lim orqali guruhingizning a'zolari @uzb_taksi_bot ga a'zo bo'lmaguncha yozdirmaydi\n\nBotni guruhingizga ulash uchun Guruhga ulash tugmasini bosing",
+        # noqa
+        reply_markup=panel_mandatory.as_markup(resize_keyboard=True)
+    )
+
+
+@buttons_router.message(Text("Majbur guruh qo'shish"))
+async def start(message: Message):
+    await message.answer(
+        "O'z guruhingizga ushbu @uzb_taksi_bot botni admin qilib barcha huquqlarni bering va guruhingiz ID raqamini yuboring yoki guruhingiz telegram manzilini yuboring."
+        # noqa
+    )
+
+
+# referal
+
+@buttons_router.message(Text("👥 Referal"))
+async def start(message: Message):
+    await message.answer(
+        """Bu havola bilan foydalanuvchilarni yig'ing va pul ishlang https://t.me/uzb_taksi_bot""",
+        # noqa
+        reply_markup=panel_referal.as_markup(resize_keyboard=True)
+    )
+
+
+# yordam
+
+@buttons_router.message(Text("🆘 Yordam"))
+async def start(message: Message):
+    await message.answer(
+        "@taksi_clone_bot botining <a href=\"https://t.me/all_nc\">TAXIDASUPPORT</a> bilan bog'lanish",
+        # noqa
+        reply_markup=back_to_main.as_markup(resize_keyboard=True)
+    )
+
+
+"""passenger"""
+
+
+@buttons_router.message(Text("🧍‍♂️ Haydovchi kerak"))
+async def start(message: Message):
+    await message.answer(
+        "Taxi turini tanlang",
+        # noqa
+        reply_markup=passenger_menu.as_markup(resize_keyboard=True)
+    )
+
+
+"""driver"""
+
+
+@buttons_router.message(Text("🚕 Haydovchiman"))
+async def start(message: Message):
+    await message.answer(
+        "😊 Assalomu alaykum hurmatli .  siz haydovchilik bo'limidasiz Yo'lovchilarni olmoqchi bo'lsangiz yo'l haqqining 5% miqdorida hisobingizda mablag' bo'lishi zarur",
+        # noqa
+        reply_markup=driver_menu.as_markup(resize_keyboard=True)
+    )
+
+
+@buttons_router.message(Text("Hisobingiz"))
+async def start(message: Message):
+    await message.answer(
+        """🆔 Hisob raqamingiz: 13261
+💳 Hisobingizda:  0.00 so'm
+
+Hisobingizni Payme yoki Click ilovalari orqali Taxida ni izlab ID raqamingizni kiritasiz va to'lov qilmoqchi bo'lgan summangizni yozib hisobni to'ldirasiz. Yoki pasdagi knopkalar orqali to'lov qilishingiz mumkin.
+
+👇🏻 Hisobingizni to'ldirish uchun kerak to'lov tizimini tanlang!""",
+        # noqa
+        reply_markup=driver_accaunt.as_markup(resize_keyboard=True)
+    )
+
+
+"""region_district"""
+
+
+@buttons_router.message(or_f(Text("Yetib borish"), Text("Yetkazib berish", Text("Yo'lovchi kutish"))))
+async def start(message: Message):
+    await message.answer(
+        "❓ Qayerdasiz?",  # noqa
+        reply_markup=region.as_markup(resize_keyboard=True)
     )
--- keyboards/default.py
+++ keyboards/default.py
@@ -1,4 +1,5 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, KeyboardButton
+import json
 
 main_menu = ReplyKeyboardBuilder([
     [KeyboardButton(text='🧍‍♂️ Haydovchi kerak')],
@@ -8,7 +9,89 @@ main_menu = ReplyKeyboardBuilder([
 ])
 main_menu.adjust(2)
 
+menu = [KeyboardButton(text='🔝 Asosiy Menyu')]
 
 back_to_main = ReplyKeyboardBuilder([
-    [KeyboardButton(text='🔝 Asosiy Menyu')]
+    menu
 ])
+
+panel_menu = ReplyKeyboardBuilder([
+    [KeyboardButton(text='💳 Balans')],
+    [KeyboardButton(text='📈 Kanal / Guruh')],
+    [KeyboardButton(text="♾ Majburiy a'zo bot")],
+    [KeyboardButton(text="👥 Referal")],
+    [KeyboardButton(text="⚙️ Sozlamalar")],
+    [KeyboardButton(text="🆘 Yordam")],
+    menu
+])
+panel_menu.adjust(2)
+
+panel_channel = ReplyKeyboardBuilder([
+    [KeyboardButton(text="Kanal qo'shish")],
+    [KeyboardButton(text="Guruh qo'shish ")],
+    menu
+])
+panel_channel.adjust(2)
+
+# majburiy
+panel_mandatory = ReplyKeyboardBuilder([
+    [KeyboardButton(text="Majbur guruh qo'shish")],
+    menu
+])
+
+panel_referal = ReplyKeyboardBuilder([
+    [KeyboardButton(text="Reklama posti")],
+    [KeyboardButton(text="Reklama post video")],
+    [KeyboardButton(text="Referallar ro'yxati")],
+    [KeyboardButton(text="🔙 Orqaga")],
+    menu
+])
+panel_referal.adjust(2)
+
+"""passenger"""
+
+passenger_menu = ReplyKeyboardBuilder([
+    [KeyboardButton(text="Yetib borish")],
+    [KeyboardButton(text="Yetkazib berish")],
+    menu
+])
+passenger_menu.adjust(2)
+
+"""driver"""
+
+driver_menu = ReplyKeyboardBuilder([
+    [KeyboardButton(text="Yo'lovchi kutish")],
+    [KeyboardButton(text="Hisobingiz")],
+    [KeyboardButton(text="Ma'lumotlarni o'zgartirish")],
+    [KeyboardButton(text="🔙 Orqaga")],
+    menu
+])
+driver_menu.adjust(2)
+
+# hisob
+driver_accaunt = ReplyKeyboardBuilder([
+    [KeyboardButton(text="CLICK")],
+    [KeyboardButton(text="PAYME")],
+    menu
+])
+
+driver_accaunt.adjust(2)
+
+"""region_district"""
+list_reg = []
+with open('/home/abror/Taxi_Bot_musharraf/region_district/reg_dis.json') as f:
+    d: dict = json.load(f)
+    for i in d.keys():
+        print(i)
+        list_reg += [[KeyboardButton(text=i)]]
+region = ReplyKeyboardBuilder(list_reg + [menu])
+region.adjust(2)
+
+
+def make_district(region):
+    with open('/home/abror/Taxi_Bot_musharraf/region_district/reg_dis.json') as f:
+        data: dict = json.load(f)
+        districts = data.get(region)
+        buttons = [[KeyboardButton(text=district)] for district in districts]
+    districts_buttons = ReplyKeyboardBuilder(buttons + [menu])
+    return districts_buttons
